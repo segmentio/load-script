5e067a4f472d6739a029ea6565f9f8865264b4ec
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-02-11 17:06:18 -0800

    initial commit

---
+++ Readme.md
@@ -0,0 +1,27 @@
+
+# load-script
+
+  Component: loads a Javascript file by appending a script tag to the DOM.
+
+## Installation
+
+    $ component install ianstormtaylor/load-script
+
+## API
+
+### loadScript(src || options, callback)
+  Load the given script either by passing a `src` string, or
+  an options object:
+
+    {
+        src: '//example.com/lib.js', // same as `src` string
+        http: 'http://example.com/lib.js', // `src` to load if the protocol is `http:`
+        https: 'https://ssl.example.com/lib.js' // `src` to load if the protocol is `https:`
+    }
+  
+  You can also pass in a `callback` that will be called when
+  the script loads successfully.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,48 @@
+var type = require('type');
+
+
+module.exports = function loadScript (options, callback) {
+    if (!options) throw new Error('Cant load nothing...');
+
+    // Allow for the simplest case, just passing a `src` string.
+    if (type(options) === 'string') options = { src : options };
+
+    var https = document.location.protocol === 'https:';
+
+    // If you use protocol relative URLs, third-party scripts like Google
+    // Analytics break when testing with `file:` so this fixes that.
+    if (options.src && options.src.indexOf('//') === 0) {
+        options.src = https ? 'https:' + options.src : 'http:' + options.src;
+    }
+
+    // Allow them to pass in different URLs depending on the protocol.
+    if (https && options.https) options.src = options.https;
+    else if (!https && options.http) options.src = options.http;
+
+    // Make the `<script>` element and insert it before the first script on the
+    // page, which is guaranteed to exist since this Javascript is running.
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = options.src;
+
+    var firstScript = document.getElementsByTagName('script')[0];
+    firstScript.parentNode.insertBefore(script, firstScript);
+
+    // If we have a callback, attach event handlers, even in IE. Based off of
+    // the Third-Party Javascript script loading example:
+    // https://github.com/thirdpartyjs/thirdpartyjs-code/blob/master/examples/templates/02/loading-files/index.html
+    if (callback && type(callback) === 'function') {
+        if (script.addEventListener) {
+            script.addEventListener('load', callback, false);
+        } else if (script.attachEvent) {
+            script.attachEvent('onreadystatechange', function () {
+                if (/complete|loaded/.test(script.readyState)) callback();
+            });
+        }
+    }
+
+    // Return the script element in case they want to do anything special, like
+    // give it an ID or attributes.
+    return script;
+};
